fcafc301b384fba38c612adc18978e27baddd154
alessandra martera <ale.martera@gmail.com>
2023-05-11 18:05:38 +0200

    stopWatch

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
 const btnStart = document.getElementById("start_game");
 const arraySimonSaysFiveNum = createArrayFiveNumRandom ();
 
+
 // Dare il via a quando stampare i numeri in pagina
 btnStart.addEventListener("click", viewSimonSays);
+// setTimeout(readNumInTotMin, 5000);
 
 
 
@@ -15,7 +17,7 @@ btnStart.addEventListener("click", viewSimonSays);
 // funzione per creare un array di 5 numeri casuali da 1 a 100
 function createArrayFiveNumRandom () {
     const arrayNumRandom = [];
-    for ( let i = 0; i < 5; i++ )  {
+    while ( arrayNumRandom.length < 5 ) {
         let numRandom = Math.floor((Math.random() * 100 ) + 1);
         arrayNumRandom.push(numRandom);
     }
@@ -27,6 +29,28 @@ function viewSimonSays () {
     // Visualizzare in pagina 5 numeri casuali.
     let elSimonSays = document.getElementById("simon_says");
     elSimonSays.innerHTML = arraySimonSaysFiveNum;
-
-
 }
+
+
+// function readNumInTotMin() {
+//     const elToDelateClass = document.querySelector("h1");
+//     elToDelateClass.classList.remove("active");
+
+// }
+
+// Funzione che fa partire il timer di 30 secondi
+const stopWatch = document.getElementById("stop_watch");
+
+let clock = 1;
+
+
+let oneSecondClock = setInterval ( addOneClock, 1000 );
+
+if (clock )
+function addOneClock () {
+    stopWatch.innerHTML = clock;
+    clock++
+}
+
+
+
